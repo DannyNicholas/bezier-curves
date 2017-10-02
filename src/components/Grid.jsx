d7b02f8510282be4bb8fcf362237fda670d618ec
DannyNicholas <dannynicholas23@googlemail.com>
2017-10-02 19:23:11 +0100

    Plot control points and add Redux devtools

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import {Layer, Stage} from 'react-konva'
 import PathPoint from './PathPoint'
+import ControlPoint from './ControlPoint'
 import './Grid.css';
 
-const Grid = ( {path} ) => {
+const Grid = ( { path, controlPoints } ) => {
     return (
         <div>
             <div>Hello</div>
@@ -16,9 +18,22 @@ const Grid = ( {path} ) => {
                         />
                     )}
                 </Layer>
+                <Layer>
+                    {controlPoints.map((point, index) =>
+                        <ControlPoint
+                            key={index}
+                            point={point}
+                        />
+                    )}
+                </Layer>
             </Stage>
         </div>
     )
+}
+
+Grid.propTypes = {
+    path: PropTypes.object.isRequired,
+    controlPoints: PropTypes.object.isRequired
 }
 
 export default Grid
--- src/store/Store.js
+++ src/store/Store.js
@@ -1,6 +1,17 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
 import GridReducer from '../reducers/GridReducer'
 
-const Store = createStore(GridReducer)
+/* For Redux dev tools see... */
+/* https://github.com/zalmoxisus/redux-devtools-extension#usage */
+const composeEnhancers = composeWithDevTools({});
+
+const Store = createStore(
+    GridReducer,
+    composeEnhancers(
+        applyMiddleware(thunk)
+    )
+)
 
 export default Store
