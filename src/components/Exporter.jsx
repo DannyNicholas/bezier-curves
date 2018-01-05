112697eb2ac8dbe0989f0c9ea6f12616ef84b7d8
DannyNicholas <dannynicholas23@googlemail.com>
2018-01-05 17:36:48 +0000

    initial attempt at json importer

--- src/components/Exporter.jsx
+++ src/components/Exporter.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import  { exportToJsonFile } from '../utils/exportToJson'
+import  { importFromJsonFile } from '../utils/importFromJson'
 import { extractPaths } from '../utils/extractPaths'
 
 const Exporter = ( {paths} ) => {
@@ -10,13 +11,52 @@ const Exporter = ( {paths} ) => {
         exportToJsonFile(json)
     }
 
+    // https://www.html5rocks.com/en/tutorials/file/dndfiles/
+
+    function handleFileSelect(evt) {
+        var files = evt.target.files; // FileList object
+    
+        // files is a FileList of File objects. List some properties.
+        var output = [];
+        for (var i = 0, f; f = files[i]; i++) {
+          output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
+                      f.size, ' bytes, last modified: ',
+                      f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
+                      '</li>');
+                      console.log(f.name)
+
+                      var reader = new FileReader();
+                      reader.readAsDataURL(f);
+                      console.log(reader)
+                      console.log(reader.result)
+
+                        // Only process image files.
+                        // console.log(f.type.match('json.*'))
+                        // if (!f.type.match('image.*')) {
+                        //     continue;
+                        // }
+
+        }
+        document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
+
+        //console.log(output)
+      }
+
     return(
         <div>
             <ul>
                 <li>
+                    <button onClick={() => importFromJsonFile()}>Import</button>
+                </li>
+                <li>
                     <button onClick={() => exportToJson()}>Export</button>
                 </li>
             </ul>
+            <div>
+            <label for="file">IMPORT</label>
+            <input type="file" id="files" name="files[]" onChange={handleFileSelect} />
+            <output id="list"></output>
+            </div>
         </div>
     )
 }
