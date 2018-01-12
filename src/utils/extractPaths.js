61de57c516f2c2f37fb4fe83a36813c672f0c26f
DannyNicholas <dannynicholas23@gmail.com>
2018-01-12 23:46:33 +0000

    Export any path type

--- src/utils/extractPaths.js
+++ src/utils/extractPaths.js
@@ -1,24 +1,35 @@
 export const extractPaths = (paths, width, height) => {
     return {
-        pathData: extractControlPoints(paths),
+        pathData: extractPathData(paths),
         width: width,
         height: height
     }
 }
 
-const extractControlPoints = (paths) => {
+// return an array of path data
+const extractPathData = (paths) => {
     return paths.map( (path) => {
-        const controlPoints = path.get('controlPoints')
-        return {
-            start: extractPoint(controlPoints.get('start')),
-            finish: extractPoint(controlPoints.get('finish')),
-            startControl: extractPoint(controlPoints.get('startControl')),
-            finishControl: extractPoint(controlPoints.get('finishControl')),
-            pathPoints: path.get('pathPoints')
-        }
+        
+        const controlPoints = extractControlPoints(path.get('controlPoints'))
+        return Object.assign(
+            { type: path.get('type') },
+            { pathPoints: path.get('pathPoints') },
+            controlPoints
+        )
     })
 }
 
+// return an object representing all control points
+const extractControlPoints = (controlPoints) => {
+
+    const extractedControlPoints = {}
+    controlPoints.forEach( (controlPoint) => {
+        extractedControlPoints[controlPoint.get('name')] = extractPoint(controlPoint)
+    })
+    return extractedControlPoints
+}
+
+// extract x and y co-ordinates from control point
 const extractPoint = (controlPoint) => {
     const point = controlPoint.get('point')
     return {
